c79e0651fc55a98914f06d8f0c76b05d93af036b
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2024-07-14 13:55:43 +0530

    using bfs

---
+++ Directed Graph Cycl.cpp
@@ -0,0 +1,37 @@
+bool isCyclic(int V, vector<int> adj[]) {
+        // code here
+        int indegree[V] = {0};
+        
+        for (int i = 0; i < V; i++) {
+			for (auto it : adj[i]) {
+				indegree[it]++;
+			}
+		}
+		
+		queue<int>q;
+		for(int i=0;i<V;i++){
+		    if(indegree[i]==0){
+		        q.push(i);
+		    }
+		}
+		
+		int cnt = 0;
+		while(!q.empty()){
+		    int node = q.front();
+		    q.pop();
+		    cnt++;
+		    
+		    for(auto it : adj[node]){
+		        indegree[it]--;
+		        if(indegree[it] == 0){
+		            q.push(it);
+		        }
+		    }
+		}
+		
+		if(cnt == V)
+		return false;
+		
+		else
+		return true;
+    }
